45251ebc6e1f2f4643bd5e8373846e233679e933
Jacob Hatfield <jacobmhatfield@gmail.com>
2022-09-24 23:00:18 -0500

    🏢 add: site architecture

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import '$assets/app.css';
+</script>
+
+<slot />
--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,47 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
+// packages
+import { mdsvex } from 'mdsvex';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from 'rehype-slug';
+
 const extensions = ['.svelte', '.md'];
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({ postcss: true }),
+	preprocess: [
+		preprocess({
+			postcss: true,
+			preserve: ['module', 'ld+json']
+		}),
+		mdsvex({
+			layout: {
+				article: './src/routes/+layout.svelte'
+			},
+			extensions: extensions,
+			rehypePlugins: [
+				// Adds 'target' and 'rel' to external links
+				rehypeExternalLinks,
+				// Adds 'id' attributes to Headings (h1, h2, etc.)
+				rehypeSlug,
+				[
+					rehypeAutolinkHeadings,
+					{
+						// Adds hyperlinks to the headings, requires rehypeSlug
+						behavior: 'append',
+						content: {
+							type: 'element',
+							tagName: 'span',
+							properties: { className: ['anchorLink'] },
+							children: [{ type: 'text', value: '#' }]
+						}
+					}
+				]
+			]
+		})
+	],
 	extensions,
 	kit: {
 		adapter: adapter()
